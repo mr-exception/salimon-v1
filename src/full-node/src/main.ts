15571b645167a53fe588d0f1992b7fa5ca7994f4
mr-exception <darbandi1996@gmail.com>
2022-06-05 13:33:17 +0430

    added process log

--- src/full-node/src/main.ts
+++ src/full-node/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env);
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
