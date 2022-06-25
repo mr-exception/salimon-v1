dd56d61060d93b6841afc6d21911f693b7ede328
mr-exception <darbandi1996@gmail.com>
2022-06-25 08:02:31 +0430

    removed cns

--- src/full-node/src/main.ts
+++ src/full-node/src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env);
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(80);
